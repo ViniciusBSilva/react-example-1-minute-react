43b9caa4da77a014de5cc0614f09d75579e341d2
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-06-30 15:38:17 -0300

    Minor changes copied from https://gist.github.com/gaearon/6668a1f6986742109c00a581ce704605

--- like_button.js
+++ like_button.js
@@ -27,4 +27,5 @@ class LikeButton extends React.Component {
 }
 
 const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
