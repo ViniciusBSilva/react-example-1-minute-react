f189e0c0fc2ca90634eaff1de4e8ae5588ba5f22
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-06-30 14:34:13 -0300

    Copy from https://pt-br.reactjs.org/docs/add-react-to-a-website.html

---
+++ like_button.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        };
+    }
+
+    render() {
+
+        if (this.props.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button', {
+                onClick: () => this.setState({
+                    liked: true
+                })
+            },
+            'Like'
+        );
+
+    }
+
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
